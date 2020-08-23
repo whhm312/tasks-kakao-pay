09c4739f28f89e17ed69b9d813f0cba69a9a2e26
whhm312 <whily312@gmail.com>
2020-08-23 16:41:52 +0900

    Add exceptions

--- kakao-pay-api/src/main/java/me/kakao/pay/common/exception/GlobalExceptionHandler.java
+++ kakao-pay-api/src/main/java/me/kakao/pay/common/exception/GlobalExceptionHandler.java
@@ -19,17 +19,33 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(FailedCreateTokenException.class)
 	protected ResponseEntity<ErrorResponse> handleDuplicatedTokenException(FailedCreateTokenException e) {
-		ErrorResponse reponse = new ErrorResponse();
-		reponse.setCode("E0001");
-		reponse.setMessage(e.getMessage());
-		return new ResponseEntity<>(reponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("E0001");
+		response.setMessage(e.getMessage());
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(FailedInsertLuckException.class)
+	protected ResponseEntity<ErrorResponse> handlerFailedInsertLuckException(FailedInsertLuckException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("E0002");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(FailedInsertLuckDetailException.class)
+	protected ResponseEntity<ErrorResponse> handlerFailedInsertLuckDetailException(FailedInsertLuckDetailException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("E0003");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		NotValiedResponse reponse = new NotValiedResponse();
-		reponse.setCode("B0001");
-		reponse.setMessage("Not valid parameters' values.");
+	protected ResponseEntity<NotValiedResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		NotValiedResponse response = new NotValiedResponse();
+		response.setCode("B0001");
+		response.setMessage("Not valid parameters' values.");
 		List<String> errors = new ArrayList<>();
 
 		BindingResult bindingResult = e.getBindingResult();
@@ -45,8 +61,65 @@ public class GlobalExceptionHandler {
 			errors.add(builder.toString());
 			builder.setLength(0);
 		}
-		reponse.setDetails(errors);
+		response.setDetails(errors);
 
-		return new ResponseEntity<>(reponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 	}
+
+	@ExceptionHandler(AlreadyGrabUserException.class)
+	protected ResponseEntity<ErrorResponse> handlerAlreadyGrabUserException(AlreadyGrabUserException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0002");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(BlesserNotAllowGrabException.class)
+	protected ResponseEntity<ErrorResponse> handlerBlesserNotAllowGrabException(BlesserNotAllowGrabException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0003");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ExpiredLuckException.class)
+	protected ResponseEntity<ErrorResponse> handlerExpiredLuckException(ExpiredLuckException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0004");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(FailedCreateTokenException.class)
+	protected ResponseEntity<ErrorResponse> handlerFailedCreateTokenException(FailedCreateTokenException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0005");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(FullGrabException.class)
+	protected ResponseEntity<ErrorResponse> handlerFullGrabException(FullGrabException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0006");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidLuckException.class)
+	protected ResponseEntity<ErrorResponse> handlerInvalidLuckException(InvalidLuckException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0007");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(NotValidMemberException.class)
+	protected ResponseEntity<ErrorResponse> handlerNotValidMemberException(NotValidMemberException e) {
+		ErrorResponse response = new ErrorResponse();
+		response.setCode("B0008");
+		response.setMessage(e.getLocalizedMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
 }
